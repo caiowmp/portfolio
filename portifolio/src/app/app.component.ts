898283040518c3f6c19fb6a872e949aadd30d25a
caiomp <caiomp@ufba.br>
2024-05-30 12:59:05 -0300

    App

--- portifolio/src/app/app.component.ts
+++ portifolio/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutComponent } from './components/about/about.component';
+import { ConnectComponent } from './components/connect/connect.component';
 
 @Component({
     selector: 'app-root',
@@ -9,7 +12,10 @@ import { HeaderComponent } from './components/header/header.component';
     styleUrl: './app.component.scss',
     imports: [
       RouterOutlet, 
-      HeaderComponent
+      HeaderComponent,
+      ProjectsComponent,
+      AboutComponent,
+      ConnectComponent
     ]
 })
 export class AppComponent {
